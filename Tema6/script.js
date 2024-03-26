fcd7260eaeacf745f7c99f824bb071d2adbb56d5
Timotei Frincu <timotei.frincu@emanuel.ro>
2024-03-26 17:17:26 +0200

    add setup and draw functions in script

---
+++ Tema6/script.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    fill(50, 100, 150);
+    rect(130, 240, 150, 150);
+    fill(255, 100, 100);
+    triangle(110, 250, 300, 250, 200, 150);
+    fill(100, 200, 250);
+    ellipse(200, 180, 20, 20);
+    line(200, 170, 200, 190);
+    line(190, 180, 210, 180);
+    fill(50, 100, 150);
+    rect(230, 170, 20, 40);
+    fill('white');
+    text('Casa de vis', 140, 240);
+    fill('brown');
+    rect(150, 320, 40, 70);
+    fill(100, 200, 250);
+    rect(200, 280, 30, 50);
+    rect(240, 280, 30, 50);
+    soare(120, 150, 20, 20);
+  }
+  
