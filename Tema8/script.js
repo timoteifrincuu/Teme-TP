6953ba3d10e671f2f7492045d64d102f4cd1633f
Timotei Frincu <timotei.frincu@emanuel.ro>
2024-04-03 12:01:29 +0300

    create setup function and matrix with object

---
+++ Tema8/script.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(400, 400);
+    a = new Array(7);
+    for (i = 0; i < 7; i++) {
+      b = new Array(4);
+      for (j = 0; j < 4; j++) {
+        b[j] = {
+          x: i,
+          y: j,
+          r: int(random(0, 255)),
+          g: int(random(0, 255)),
+          b: int(random(0, 255)),
+          afiseaza: function afiseaza() {
+            stroke(0, 0, 0);
+            strokeWeight(1);
+            fill(this.r, this.g, this.b);
+            square(20 + this.x * 30, 20 + this.y * 30, 25);
+            textSize(5);
+            text(this.x + 1 + this.y * 7, 30 + this.x * 30, 35 + this.y * 30);
+          },
+        };
+      }
+      a[i] = b;
+    }
+  }
