a290f91c06670800857ce4d22e8aab89508b24d0
Timotei Frincu <timotei.frincu@emanuel.ro>
2024-04-17 21:10:04 +0300

    create setup function, initializing the array with null values

--- Tema9/script.js
+++ Tema9/script.js
@@ -1,8 +1,21 @@
+
 let board = [];
 let pawn = function(x,y,col){
   this.x = x;
   this.y = y;
   this.col = col;
 }
+
+function setup(){
+  createCanvas(400,400);
+  for(let i = 0; i < 8; i++){
+    board[i] = [];
+    for (let j = 0; j < 8; j++){
+      board[i][j] = null;
+    }
+  }
+}
+
+
 
 //dont forget to make commits after each important thing!
