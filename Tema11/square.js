de3978d6d69745d0be796a9fc64b9ddfe56d3ad8
Timotei Frincu <timotei.frincu@emanuel.ro>
2024-05-24 17:41:57 +0300

    create class square with constructor and functions

--- Tema11/square.js
+++ Tema11/square.js
@@ -1,4 +1,29 @@
-class square{
+let Squares = [];
+ 
+class Square{
 
-    
+    constructor() {
+        this.size = 400;
+        this.rotation = 0;
+    }
+
+    update(){
+        this.size--;
+        this.rotation--;
+    }
+
+    show(){
+        push()
+        translate(innerWidth/2, innerHeight/2)
+        rotate(this.rotation);
+        noFill();
+        strokeWeight(2);
+        rect(0,0,this.size,this.size)
+        pop()
+    }
+
+    stop(){
+        return this.size < 1;
+    }
+
 }
